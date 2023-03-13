45516c62eb12881af006956f4942416961644a3e
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-13 14:31:16 +0100

    Case 4 - not an int

--- test_primefactors.py
+++ test_primefactors.py
@@ -4,6 +4,10 @@ NUMBERS = [
     (3, [1, 3])
 ]
 
+DATA = [
+    (1.1, "Not an integer")
+]
+
 
 def prime_factors(number):
     factors = []
@@ -12,9 +16,21 @@ def prime_factors(number):
     if number == 2:
         factors.append(1)
         factors.append(2)
+    if number == 3:
+        factors.append(1)
+        factors.append(3)
     return factors
 
 
 def test_prime_factors():
     for test_case, expected in NUMBERS:
         assert prime_factors(test_case) == expected
+
+
+def test_prime_factors_wrong_data():
+    for test_case, expected in DATA:
+        try:
+            prime_factors(test_case)
+        except ValueError as e:
+            assert e.args[0] == expected
+
