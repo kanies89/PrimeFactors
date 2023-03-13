32119d207a5f628023da49a650d83ce19f4e1b90
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-13 14:03:44 +0100

    Case 1

---
+++ test_primefactors.py
@@ -0,0 +1,13 @@
+NUMBERS = [
+    (1, 1)
+]
+
+
+def prime_factors(number):
+    if number == 1:
+        return 1
+
+
+def test_prime_factors():
+    for test_case, expected in NUMBERS:
+        assert prime_factors(test_case) == expected
