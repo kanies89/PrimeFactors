95c4bfccfb5518ff4a7e1fdff78f32e922a307bc
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-13 17:40:19 +0100

    Next Case - prime as factor added

--- test_primefactors.py
+++ test_primefactors.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 NUMBERS = [
     (1, [1]),
     (2, [1, 2]),
@@ -7,37 +9,50 @@ NUMBERS = [
     (6, [1, 2, 3]),
     (7, [1, 7]),
     (8, [1, 2, 2, 2]),
-    [49, [1, 7, 7]]
+    (49, [1, 7, 7]),
+    (2135, [1, 5, 7, 61]),
+    (6789, [1, 3, 31, 73])
 ]
 
 DATA = [
-    (1.1, "Not an integer")
+    (1.1, "Not an integer"),
+    ("Prime", "Not an integer")
 ]
 
 primes = []
 
+
+def calculate_primes(num_given):
+    global primes
+    for num in range(3, num_given + 1):
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+            else:
+                primes.append(num)
+                primes = list(set(primes))
+
+
 def prime_factors(number):
+    global primes
     if not isinstance(number, int):
         return "Not an integer"
+
     factors = []
     if number >= 1:
         factors.append(1)
     while number > 1:
         if number % 2 == 0:
             factors.append(2)
-            number = number / 2
+            number /= 2
         if number % 2 != 0 and number != 1:
-            no_prime_factor = True
-            if len(primes) > 0:
-                for p in primes:
-                    if number % p == 0:
-                        no_prime_factor = False
-                        factors.append(p)
-                        number = number / p
-            if no_prime_factor:
-                primes.append(number)
-                factors.append(number)
-                number = number / number
+            calculate_primes(int(number))
+
+            for p in primes:
+                if number % p == 0:
+                    factors.append(p)
+                    number /= p
+
     return factors
 
 
@@ -52,4 +67,3 @@ def test_prime_factors_wrong_data():
             prime_factors(test_case)
         except ValueError as e:
             assert e.args[0] == expected
-
